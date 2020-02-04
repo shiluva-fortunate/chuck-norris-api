f48d2e8df26ff6dede7c64917b4d93d3bcd4979c
shiluva-fortunate <shiluva1202@gmail.com>
2020-02-04 12:24:19 +0200

    styled-components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import { Grid } from 'react-mdl';
 import logo from './assets/logo.png';
 import './App.css';
-
 import Catagories from './components/categories';
 import Joke from './components/joke';
+import styled from 'styled-components';
+
+//this is just a snippet of code that exhibits styled-components
+const Shiluva = styled.div`
+    color: #ff4081;
+    background-color: #fff;
+    padding: 10px 5px;
+    width: 120px;
+    border-radius: 5px;
+    text-transform: capitalize;
+    margin-left: 5%;
+    margin-top: 2%;
+`;
 
 export default () => {
   return (
     <div className='landing'>
-      <p className='badge'>Chuck Norris</p>
+      <Shiluva>
+        Chuck Norris
+      </Shiluva>;
       <div className='header'>
         <div className='logo-container'>
           <img src={logo} className='App-logo' alt='logo' />
